cca99061e5109b2d8cb0b19837528cb8560d4401
SametKiybik <samettemas05@gmail.com>
2023-05-22 11:27:35 +0300

    back

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+window.addEventListener('load', function() {
+    setTimeout(function() {
+      document.querySelector("#gecis").style.visibility="visible"
+    }, 2000); // 2000 milisaniye (2 saniye) gecikme süresi
+  });
